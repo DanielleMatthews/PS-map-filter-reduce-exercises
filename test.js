9fc38401ff4982c41ff98423cd332562ad371b33
DanielleMatthews <danielleem120@gmail.com>
2022-04-10 20:44:07 -0400

    homework

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-/*
-Map Transformers
-Using map, return an array of each transformer's value of its 'form' property 
-*/
-//result: ['Freightliner Truck','Gun', 'VW Beetle','Walkman']
+// /*
+// Map Transformers
+// Using map, return an array of each transformer's value of its 'form' property 
+// */
+// //result: ['Freightliner Truck','Gun', 'VW Beetle','Walkman']
+console.log(' #1.')
 const transformersMap = () => {
     let transformers = [
         {
@@ -26,24 +27,31 @@ const transformersMap = () => {
           team: 'Decepticon'
         }
       ];
+      const forms = transformers.map(transformer=>{
+        return transformer.form
+      })
+      console.log(forms)
 }
-/*
-Filter Transformers
-Using filter, return an array of transformer objects that have the 'team' property of 'Autobot'
+transformersMap()
 
-result: [ {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-         {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        }
-    ]
+//     /*
+// Filter Transformers
+// Using filter, return an array of transformer objects that have the 'team' property of 'Autobot'
 
-*/
+// result: [ {
+//           name: 'Optimus Prime',
+//           form: 'Freightliner Truck',
+//           team: 'Autobot'
+//         },
+//          {
+//           name: 'Bumblebee',
+//           form: 'VW Beetle',
+//           team: 'Autobot'
+//         }
+//     ]
+
+// */
+console.log(' #2.')
 const transformersFilter = () => {
     let transformers = [
         {
@@ -67,41 +75,57 @@ const transformersFilter = () => {
           team: 'Decepticon'
         }
       ]
+      const autobots = transformers.filter(
+        (transformer) => transformer.team === 'Autobot'
+      )
+      console.log(autobots)
+}
+transformersFilter()
 
-}
+// /*
+// Reduce Transformers
+// Using reduce, return an object that has a count for each team of transformer
+// result: {
+//     Autobot: 2,
+//     Decepticon: 2
+// }
+// */
+// const reduceTransformers = () => {
+//     let transformers = [
+//         {
+//           name: 'Optimus Prime',
+//           form: 'Freightliner Truck',
+//           team: 'Autobot'
+//         },
+//         {
+//           name: 'Megatron',
+//           form: 'Gun',
+//           team: 'Decepticon'
+//         },
+//         {
+//           name: 'Bumblebee',
+//           form: 'VW Beetle',
+//           team: 'Autobot'
+//         },
+//         {
+//           name: 'Soundwave',
+//           form: 'Walkman',
+//           team: 'Decepticon'
+//         }
+//       ]
+//       const autosAndDecepticon = transformers.reduce((acc, cur) =>{
+//        const teams = cur.team
+//        return acc
+//       //  if(acc[teams]){
+//       //    acc[teams] = acc[teams] +1;
+//       //  } else{
+//       //    acc[teams] = 1
+//       //  }
+//       })
+//       // console.log(autosAndDecepticon)
+// }
+// reduceTransformers()
 
-/*
-Reduce Transformers
-Using reduce, return an object that has a count for each team of transformer
-result: {
-    Autobot: 2,
-    Decepticon: 2
-}
-*/
-const reduceTransformers = () => {
-    let transformers = [
-        {
-          name: 'Optimus Prime',
-          form: 'Freightliner Truck',
-          team: 'Autobot'
-        },
-        {
-          name: 'Megatron',
-          form: 'Gun',
-          team: 'Decepticon'
-        },
-        {
-          name: 'Bumblebee',
-          form: 'VW Beetle',
-          team: 'Autobot'
-        },
-        {
-          name: 'Soundwave',
-          form: 'Walkman',
-          team: 'Decepticon'
-        }
-      ]
-}
 
 
 
@@ -113,11 +137,18 @@ Use filter and reduce
 */
 
 //result: 42
+console.log(' #3.')
 const sumPositiveElement = () => {
     const input = [ 1, -4, 12, 0, -3, 29, -150]
+    const positive = input.filter(
+       (input) => input >= 0
+    )
+    .reduce((acc, cur) => {
+      return acc + cur
+    }, 0) //set acc to starting value
+    console.log(positive)
 }
-
-
+sumPositiveElement()
 
 /* 
 Calculate median and mean
@@ -125,21 +156,35 @@ Calculate the mean and median values of the number elements from the input array
 
 Use: reduce, sort, Math.abs
 */
-//result: { mean: 38.5, median: 32 }
+//result: { mean: 38.5, median: 32 } //get total / all - find closest # to mean
+console.log(' #4.')
 const medianMean = () => {
     const input = [12, 46, 32, 64]
+    // input.sort((a, b) => a-b)
+    const means = input.reduce((acc, cur, index, array) =>{
+      // console.log(acc, cur, array.length)
+      return ((acc += cur) / array.length)
+    }, 0)
+    console.log(means)
 }
-
+medianMean()
 
 /*
 Get name initials
 The given input is a string of multiple words with a single space between each of them. Abbreviate the name and return the name initials.
 Use .map , .split , .join
 */
+
 //result: 'GRRM'
+console.log(' #5.')
 const nameInitials = () => {
-    const input = 'George Raymond Richard Martin';
+    const input = 'George Raymond Richard Martin'
+    .split(' ')
+    .map(name => name[0])
+    .join('')
+  console.log(input)
 }
+nameInitials()
 
 /*
 Age difference from the youngest and oldest
@@ -148,6 +193,7 @@ Use .map , Math.min , Math.max
 */
 
 //Result: [13, 67, 54]
+console.log(' #6.')
 const ageDifference = () => {
     const input = [
         {
@@ -171,5 +217,18 @@ const ageDifference = () => {
           age: 65,
         }
       ];
+      const max = Math.max.apply(Math, input.map(data=>{
+        return (data.age)
+      }))
+      const min = Math.min.apply(Math, input.map(data=>{
+        return (data.age)
+      }))
+      const diff = max - min
+      console.log(min, max, diff)
 }
+ageDifference()
 
+//  const forms = transformers.map(transformer=>{
+//         return transformer.form
+//       })
+//       console.log(forms)
